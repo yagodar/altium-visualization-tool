d6e3b9fe3f15a01a47a11f672843906468cd3ae5
Azazelle <aza.conf@bk.ru>
2013-05-14 20:38:47 +0400

    вернул назад последний коммит

--- src/ru/ifmo/avt/browser/GUI/components/BrowserToolBar.java
+++ src/ru/ifmo/avt/browser/GUI/components/BrowserToolBar.java
@@ -33,19 +33,19 @@ public class BrowserToolBar extends JToolBar {
 	scalePanel.add(scaleSpinner, new GBC(1, 0));
 	
 	JLabel frequencyLabel = new JLabel("  F max [Hz]: ");
-	scalePanel.add(frequencyLabel, new GBC(0, 1));
+	scalePanel.add(frequencyLabel, new GBC(2, 0));
 	
 	BrowserFrequencySpinner frequencySpinner = new BrowserFrequencySpinner();
-	scalePanel.add(frequencySpinner, new GBC(1, 1));
+	scalePanel.add(frequencySpinner, new GBC(3, 0));
 	
 	JLabel accelerationLabel = new JLabel("  Ao [m/s^2]: ");
-	scalePanel.add(accelerationLabel, new GBC(0, 2).setAnchor(GBC.NORTHWEST));
+	scalePanel.add(accelerationLabel, new GBC(4, 0));
 	
 	BrowserAccelerationSpinner accelerationSpinner = new BrowserAccelerationSpinner();
-	scalePanel.add(accelerationSpinner, new GBC(1, 2).setAnchor(GBC.NORTHWEST));
+	scalePanel.add(accelerationSpinner, new GBC(5, 0));
 	
 	JLabel fixingLabel = new JLabel("  Fixing: ");
-	scalePanel.add(fixingLabel, new GBC(2, 0, 1, 3));
+	scalePanel.add(fixingLabel, new GBC(6, 0));
 	
 	File file = new File(".\\res");
 
@@ -65,9 +65,9 @@ public class BrowserToolBar extends JToolBar {
 		}
 	
 	BrowserFixingList fixingList = new BrowserFixingList(icons);
-	scalePanel.add(fixingList, new GBC(3, 0, 1, 3));
+	scalePanel.add(fixingList, new GBC(7, 0));
 	
-	scalePanel.add(new JPanel(), new GBC(4, 0, 1, 3).setFill(GBC.HORIZONTAL).setWeight(100, 100));
+	scalePanel.add(new JPanel(), new GBC(8, 0).setFill(GBC.HORIZONTAL).setWeight(100, 100));
 	
 	add(scalePanel);
 	
